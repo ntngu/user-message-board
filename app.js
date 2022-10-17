41a9f5c497b06345ff2e9c95e13bac9b64731e1e
Nhan Nguyen <nhantnguyen42@gmail.com>
2022-10-17 09:01:57 -0700

    Added dependencies and .env.

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require("dotenv").config();
+console.log(process.env);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var compression = require("compression");
+var helmet = require("helmet");
+
 var app = express();
+
+var mongoose = require("mongoose");
+var mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error."));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +28,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
