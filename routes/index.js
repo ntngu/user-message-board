6e0331023bd1afe2374015fc207a0ef5a0be624f
Nhan Nguyen <nhantnguyen42@gmail.com>
2022-10-19 00:15:59 -0700

    Added user log-in and log-out.

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,20 @@ var passport = require("passport");
 var messageController = require("../controllers/messageController");
 var userController = require("../controllers/userController");
 
-/* GET home page. */
+/** GET home page. */
 router.get("/", (req, res) => {
   res.render("index", { user: req.user });
 });
 
-/* GET and POST signup */
+/** GET and POST signup */
 router.get("/sign-up", userController.user_create_get);
 router.post("/sign-up", userController.user_create_post);
 
-/* GET and POST message */
+/** GET and POST message */
 router.get("/post", messageController.message_create_get);
 router.post("/post", messageController.message_create_post);
 
+/** GET and POST log-in*/
 router.get("/log-in", userController.user_login_get);
 router.post(
   "/log-in",
@@ -25,5 +26,15 @@ router.post(
     failureRedirect: "/",
   })
 );
+
+/** GET log-out */
+router.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
